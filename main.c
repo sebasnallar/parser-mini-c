6e9b72723e04f5a9802d45e4420c049ebf17e282
Sebastian Nallar <sebastiansaidnallar@gmail.com>
2025-02-18 21:28:14 -0300

    include compilation errors in success

--- main.c
+++ main.c
@@ -6,18 +6,21 @@ extern int yyparse(void);
 extern int errlex;
 extern int errsint;
 
-typedef void (*error_handler)(void);
+typedef void (*compilation_status_handler)(void);
 
 void print_error_summary(void) {
     printf("Errores de compilación\n");
     printf("Errors sintácticos: %d - Errores léxicos: %d\n", errsint, errlex);
 }
 
-void do_nothing(void) {}
+void print_success(void) {
+    printf("Compilación terminada con éxito\n");
+    printf("Errors sintácticos: %d - Errores léxicos: %d\n", errsint, errlex);
+}
 
 int main(void) {
     int resultado = yyparse();
-    error_handler handlers[] = {do_nothing, print_error_summary};
+    compilation_status_handler handlers[] = {print_success, print_error_summary};
     handlers[(errlex > 0 || errsint > 0)]();
     return resultado;
 }
