3f617732ae7721a18eff5fe35bad2a5aa8c0bd50
Sebastian Nallar <sebastiansaidnallar@gmail.com>
2025-02-18 21:26:01 -0300

    avoid if

--- main.c
+++ main.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include "parser.tab.h"
 
-extern FILE* yyin;
 extern int yyparse(void);
-extern int line_number;
 extern int errlex;
 extern int errsint;
 
+typedef void (*error_handler)(void);
+
+void print_error_summary(void) {
+    printf("Errores de compilación\n");
+    printf("Errors sintácticos: %d - Errores léxicos: %d\n", errsint, errlex);
+}
+
+void do_nothing(void) {}
+
 int main(void) {
     int resultado = yyparse();
-    if (errlex > 0 || errsint > 0) {
-        printf("Errores de compilación\n");
-        printf("Errors sintácticos: %d - Errores léxicos: %d\n", errsint, errlex);
-    }
+    error_handler handlers[] = {do_nothing, print_error_summary};
+    handlers[(errlex > 0 || errsint > 0)]();
     return resultado;
 }
